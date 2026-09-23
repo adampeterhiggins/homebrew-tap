6472f667ceda654d276f2c0311a1bec194748d1b
Adam Higgins <adampeterhiggins@gmail.com>
2026-09-23 17:48:41 +0100

    github-monitor 0.2.4

--- Casks/github-monitor.rb
+++ Casks/github-monitor.rb
@@ -1,6 +1,6 @@
 cask "github-monitor" do
-  version "0.2.3"
-  sha256 "7788b9d132f74ac7371ef23fcda45519edc15eaca5781f4433563e6e50df4ee5"
+  version "0.2.4"
+  sha256 "3a8771306c57a1013fe067a8965dd45771cb7e16c40710cf76516dc74a4574ca"
 
   url "https://github.com/adampeterhiggins/github-monitor/releases/download/v#{version}/github-monitor_#{version}_universal.dmg"
   name "GitHub Monitor"
